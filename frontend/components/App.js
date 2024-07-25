136b5f1976c3e2a54143ea82f6000df0ff656b75
jesusmedero <jjmowd@gamil.com>
2024-07-24 19:43:13 -0700

    Sprint Challenge 6

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -8,11 +8,50 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+  const [characters, setCharacters] = useState([])
+  const [planets, setPlanets] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [peopleResponse, planetsResponse] = await Promise.all([
+          axios.get(urlPeople),
+          axios.get(urlPlanets)
+        ])
+        
+        const peopleData = peopleResponse.data
+        const planetsData = planetsResponse.data
+
+        
+        const planetMap = planetsData.reduce((map, planet) => {
+          map[planet.id] = planet.name
+          return map
+        }, {})
+
+    
+        const updatedCharacters = peopleData.map(character => ({
+          ...character,
+          homeworld: planetMap[character.homeworld] || 'Unknown'
+        }))
+
+        setCharacters(updatedCharacters)
+        setPlanets(planetsData)
+      } catch (error) {
+        console.error('Error fetching data', error)
+      }
+    }
+
+    fetchData()
+  }, [])
+  
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {characters.map(character => (
+        <Character key={character.id} character={character} />
+      ))}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character({ character }) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const [showPlanet, setShowPlanet] = useState(false);
+
+  
+  const CardClick = () => {
+    setShowPlanet(prevState => !prevState);
+  };
+
   return (
-    <div>
+    <div className="character-card" onClick={CardClick}>
       {/* Use the same markup with the same attributes as in the mock */}
+      <h3 className="character-name">{character.name}</h3>
+      {showPlanet && (
+        <p>
+        Planet: <span class="character-planet">{character.homeworld}</span>
+        </p>
+      )}
     </div>
   )
 }
